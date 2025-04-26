d0d6a9855befa192aa8c1f9985c4f4c06f3a8a5b
bilgehangul <bilge.gul.us@gmail.com>
2025-04-26 14:12:23 -0400

    logo changed

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
+import logo from '../../assets/logo.png';
 
 const Navbar: React.FC = () => {
     return (
         <nav className="navbar">
             <div className="navbar-brand">
-                <Link to="/"><h1>Bilgehan Gul</h1></Link>
+                <Link to="/">
+                    <img src={logo} alt="Bilgehan Gul" className="navbar-logo" />
+                    <h1>Bilgehan Gul</h1>
+                </Link>
             </div>
             <div className="navbar-links">
                 <Link to="/about">About</Link>
